7e0ed3d3e7b94b2b10688fc2ed2306d356565caa
NaaMuu <kk0506014@gmail.com>
2024-11-19 17:13:37 +0900

    5 step complete

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -10,18 +10,39 @@ public class Main {
 
 class App {
     public void run() {
+        Scanner sc = new Scanner(System.in);
+        String[] strs = new String[2];
+        String[] names = new String[2];
+        int count = 0;
+
         System.out.println("== 명언 앱 ==");
 
-        Scanner scanner = new Scanner(System.in);
-
         while (true) {
-            System.out.println("명령) ");
-            String cmd = scanner.nextLine();
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
 
-            if (cmd.equals("종료")) {
+            if (cmd.equals("등록")  && count < 2) {
+                System.out.print("명언 : ");
+                strs[count] = sc.nextLine();
+                System.out.print("작가 : ");
+                names[count] = sc.nextLine();
+                System.out.println((count+1) + "번 명언이 등록되었습니다.");
+                count++;
+            }
+
+            else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                while (count > 0) {
+                    System.out.println(count + " / " + names[count-1] + " / " + strs[count-1]);
+                    count--;
+                }
+            }
+
+            else if (cmd.equals("종료")) {
                 break;
             }
         }
-        scanner.close();
+        sc.close();
     }
 }
